81d037b69032fbdf2ff4c5c3984abcc12b473a44
Paulo Henrique de Oliveira <pauloh2004@gmail.com>
2026-03-25 15:19:11 -0300

    corrigndo cors para o frontend

--- src/main/java/br/com/paulooliveira/pagamentos/controller/PagamentosController.java
+++ src/main/java/br/com/paulooliveira/pagamentos/controller/PagamentosController.java
@@ -1,15 +1,13 @@
 package br.com.paulooliveira.pagamentos.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/payments")
+@CrossOrigin(origins = "https://calm-rock-03d070d0f.1.azurestaticapps.net")
 public class PagamentosController {
 
 	@GetMapping
