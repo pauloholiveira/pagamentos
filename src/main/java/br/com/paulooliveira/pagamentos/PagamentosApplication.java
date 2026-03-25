ef5cc71bd0fa80a2281d52daa65decef9b087565
Paulo Henrique de Oliveira <pauloh2004@gmail.com>
2026-03-24 23:14:53 -0300

    Criando primeiro endpoint

--- src/main/java/br/com/paulooliveira/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/paulooliveira/pagamentos/PagamentosApplication.java
@@ -2,8 +2,10 @@ package br.com.paulooliveira.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.paulooliveira.pagamentos")
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/paulooliveira/pagamentos/controller/PagamentosController.java
@@ -0,0 +1,21 @@
+package br.com.paulooliveira.pagamentos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/payments")
+public class PagamentosController {
+
+	@GetMapping
+	@ResponseBody
+	public Map<String, List<String>> list() {
+	    return Map.of("pagamentos", List.of("pagamento1", "pagamento2"));
+	}
+}
