0f934a05e175a763b2c8dcd3a27965d1618f06f0
Paulo Henrique de Oliveira <pauloh2004@gmail.com>
2026-03-25 14:42:22 -0300

    removendo import desnecessario

--- src/main/java/br/com/paulooliveira/pagamentos/controller/PagamentosController.java
+++ src/main/java/br/com/paulooliveira/pagamentos/controller/PagamentosController.java
@@ -1,13 +1,12 @@
 package br.com.paulooliveira.pagamentos.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/payments")
